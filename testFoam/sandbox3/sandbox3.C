38165b9ab57e740bd029332214cb92118e8acd91
sumeifang <sumeifang522@gmail.com>
2024-01-17 19:53:11 +0800

    use template

--- testFoam/sandbox3/sandbox3.C
+++ testFoam/sandbox3/sandbox3.C
@@ -33,99 +33,34 @@ Description
 #include "fvCFD.H"
 #include "linearEqn.H"
 #include "cubicEqn.H"
+#include "templateDemo.H"
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 int main(int argc, char *argv[])
 {
-    //#include "setRootCase.H"
-    //#include "createTime.H"
 
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-    //Info<< nl;
-    //runTime.printExecutionTime(Info);
-
-    //Info<< "End\n" << endl;
-
-   /* Foam::Info << "hi" << Foam::endl;
-    Info << "hi,i am using the foam namespace" << endl;*/
-    
-    
-    //#include "linearEqn.H"如果把头文件放在这会出现很多错误
-    scalar a = 5.42;
-    scalar b = 4.23;
-
-    Info << "a+b = " << a+b << endl;
+    //#include "foamNamespaceDemo.H"
+    //#include "linearDemo.H"
+    //#include "cubicDemo.H"
+    //#include "runTimeDemo.H"
 
     
-    //对应头文件中的这段构造函数inline linearEqn(const scalar a, const scalar b);
-    linearEqn eq1(2,7);//给a和b赋值并代入eq1， a*x + b = 0
+     float a = 2.23;
+    // float b = returnNumber(a);
+    float b = returnNumber<float>(a);//使用template
+    Info << "b is " << b << endl;
 
-    Info << "y = 2x + 7" << endl;
-    //下面是为了求解y值，x = 5
-    a = eq1.value(5);//使用了这个构造函数： inline scalar value(const scalar x) const;
-    
-    Info << "at x = 5 ,y = " << a << endl;
+    int d=5.65;
+    //int e=returnNumber(d);
+    int e=returnNumber<int>(d);//使用template
+    Info << "e is " << e << endl;
 
-    //求导数get derivative
-    Info << "dy/dx at x=5 is " << eq1.derivative(5) << endl;
-    //roots求根
-    Info << "root " << eq1.roots() << endl;
+    scalar scalar1;
+    scalar1=10.2;
+    scalar scalar2=returnNumber<scalar>(scalar1);
+    Info << "scalar value is " << scalar2 << endl;
 
 
-    //a*x^3 + b*x^2 + c*x + d = 0
-    cubicEqn eq2(1,2,3,4);
-    b = eq2.value(7);
-    Info << "at x = 7 ,y = " << b << endl;
-    //求导数get derivative
-    Info << "dy/dx at x=0 is " << eq2.derivative(0) << endl;
-    Info << "dy/dx at x=7 is " << eq2.derivative(7) << endl;
-    Info << "roots " << eq2.roots() << endl;
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-    // argList::addNote
-    //(
-    //    "Transient solver for incompressible, laminar flow"
-    //    " of Newtonian fluids."
-    //);
-
-    //#include "postProcess.H"
-
-    //#include "addCheckCaseOptions.H"
-    #include "setRootCaseLists.H"
-    #include "createTime.H"
-    //#include "createMesh.H"
-
-    /*pisoControl piso(mesh);
-
-    #include "createFields.H"
-    #include "initContinuityErrs.H"*/
-
-    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-    Info<< "\nStarting time loop\n" << endl;
-
-    while (runTime.loop())
-    {
-        Info<< "Time = " << runTime.timeName() << nl << endl;
-
-       /* #include "CourantNo.H"
-
-        // Momentum predictor
-        #include "UEqn.H"
-        // --- PISO loop
-        while (piso.correct())
-        {  
-            #include "pEqn.H"         
-        }
-         #include "TEqn.H"*/
-        runTime.write();//表示循环几次后写出结果
-        Info << runTime.path() << endl;//输出了路径
-
-        runTime.printExecutionTime(Info);
-    }
-
-    Info<< "End\n" << endl;
 
     return 0;
 }
